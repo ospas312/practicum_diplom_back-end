4680087082327b19567b4f4616f5d51ec26bb66f
Andrey Ospennikov <ospas312@ya.ru>
2020-06-05 00:48:24 +0300

    edit reg password

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,13 @@ const { createUser, login } = require('../controllers/users');
 routers.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().pattern(/^[a-zA-Z0-9!@#$%^&]{8,30}$/),
+    password: Joi.string().required().pattern(/^[a-zA-Z0-9!@#$%^&()]{8,30}$/),
   }),
 }), login);
 routers.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().pattern(/^[a-zA-Z0-9!@#$%^&]{8,30}$/),
+    password: Joi.string().required().pattern(/^[a-zA-Z0-9!@#$%^&()]{8,30}$/),
     name: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
